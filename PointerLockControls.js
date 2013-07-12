7508085aa5e4d58d2ba626fc94006b08c406f7b8
Manny <palouman@gmail.com>
2013-07-11 20:45:13 -0400

    rotation

--- PointerLockControls.js
+++ PointerLockControls.js
@@ -154,8 +154,8 @@ THREE.PointerLockControls = function ( camera ) {
 		if ( moveRight ) velocity.x += 0.12 * delta;
 
 
-		if ( turnLeft ) velocity.x -= 0.24 * delta;
-		if ( turnRight ) velocity.x += 0.24 * delta;
+		if ( turnLeft ) rotation.x -= 0.24 * delta;
+		if ( turnRight ) rotation.x += 0.24 * delta;
 
 
 		if ( isOnObject === true ) {
